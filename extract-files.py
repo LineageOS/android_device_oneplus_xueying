77234b9269d95175560fc8ca926129e63f6d5a6a
Bruno Martins <bgcngm@gmail.com>
2026-05-30 15:09:24 +0100

    xueying: De-blob some oplus interfaces
    Change-Id: I2adf8ba6301186aac8691c9cb4c1229658866c34

--- extract-files.py
+++ extract-files.py
@@ -35,7 +35,6 @@ lib_fixups: lib_fixups_user_type = {
     (
         'libRGBC',
         'libhwconfigurationutil',
-        'vendor.oplus.hardware.cammidasservice-V1-ndk',
     ): lib_fixup_vendor_suffix,
 }
 
