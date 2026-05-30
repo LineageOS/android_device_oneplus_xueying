aab4f1db667f7e8bc8a191e3093e150f082a26c8
Bruno Martins <bgcngm@gmail.com>
2026-05-30 15:05:42 +0100

    xueying: Update from OOS 11.H.20
    Change-Id: I6176368535244605723a7b03badd2fa0938c91ca

--- lineage_xueying.mk
+++ lineage_xueying.mk
@@ -23,8 +23,8 @@ PRODUCT_MODEL := CPH2551
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="qssi-user 16 BP2A.250605.015 1772545596217 release-keys" \
-    BuildFingerprint=OnePlus/CPH2551EEA/OP5973L1:16/BP2A.250605.015/T.R4T3.d34d4a_1599d02_14ea436:user/release-keys \
+    BuildDesc="qssi-user 16 BP2A.250605.015 1778234367138 release-keys" \
+    BuildFingerprint=OnePlus/CPH2551EEA/OP5973L1:16/BP2A.250605.015/T.R4T3.428cbbb-1ecba34-1ecb77a:user/release-keys \
     DeviceName=OP5973L1 \
     DeviceProduct=CPH2551 \
     SystemDevice=OP5973L1 \
